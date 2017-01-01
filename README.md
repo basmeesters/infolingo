c107063c29c2df51283648c412f99160877d3d2f
basmeesters <meesters.bas@gmail.com>
2017-01-01 15:49:44 +0100

    Add Pandoc & Test framework setup

---
+++ README.md
@@ -0,0 +1,20 @@
+Infolingo
+=========
+
+Introduction
+------------
+InfoLingo is a small DSL allowing users to create their own wiki. It allows the
+user to focus on the content, rather than the layout. A wiki is automatically
+generated from the given articles.
+
+How to build
+------------
+TODO
+
+How to use
+----------
+TODO
+
+Requirements
+------------
+TODO
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib(
+    someFunc
+) where
+
+import Text.Pandoc
+import Text.Pandoc
+import Text.Pandoc.Walk (walk)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = interact (writeDoc . walk behead . readDoc)
+
+behead :: Block -> Block
+behead (Header n _ xs) | n >= 2 = Para [Emph xs]
+behead x = x
+
+readDoc :: String -> Pandoc
+readDoc s = case readMarkdown def s of
+    Right doc -> doc
+    Left err  -> error (show err)
+
+writeDoc :: Pandoc -> String
+writeDoc doc = writeMarkdown def doc
+
+doc :: String
+doc = "*hi*"
